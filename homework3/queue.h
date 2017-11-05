5752b8fcb497203db55a9e3522c73d295e3b28a9
track394 <track394@yahoo.com>
2017-11-04 22:34:59 -0700

    Fixed enqueue and dequeue, got tests working.

--- homework3/queue.h
+++ homework3/queue.h
@@ -1,77 +1,86 @@
 #include <iostream>
+#include <cassert>
+
 using namespace std;
+
 
 template<typename T>
 class Node {
-private:
 	T data;
 	Node* next;
 
 public:
-		explicit Node(const T& d): data(d), next(nullptr) {}
+	explicit Node(const T& d): data(d), next(nullptr) {}
 
-	  Node(const Node&) = delete;
+	~Node() { delete next; }
 
-	  Node& operator= (const Node&) = delete;
+	void change_next(Node* n) {
+		next = n;
+	}
 
-		~Node() { delete next; }
+	Node* get_next() {
+		return next;
+	}
 
-		void change_next(Node* n) {
-			next = n;
-		}
-
-		Node* get_next() {
-			return next;
-		}
-
-		T get_data() {
-			return data;
-		}
-
+	T get_data() {
+		return data;
+	}
 };
 
 template<typename T>
 class Queue {
-private:
+public:
  	Node<T>* startNode;
  	Node<T>* endNode;
 	int size;
 
-public:
 	explicit Queue(): startNode(nullptr), endNode(nullptr), size(0) {}
 
-  Queue(const Queue&) = delete;
+    Queue(const Queue&) = delete;
 
-  Queue& operator= (const Queue&) = delete;
+    Queue& operator=(const Queue&) = delete;
 
 	~Queue() {
-	    delete startNode;
-			delete endNode;
+		delete startNode;
 	}
 
 	void enqueue(const T& value) {
-      Node<T>* newNodePointer = new Node<T>(value);
+	    //cout << "starting enqueue" << endl;
+			Node<T>* newNodePointer = new Node<T>(value);
+	    //cout << "made a node" << endl;
       if (size > 0) {
-          newNodePointer->change_next(startNode);
-      }
-			startNode = newNodePointer;
+  	    //cout << "okay bigger than zero, updating end node's next field" << endl;
+				endNode->change_next(newNodePointer);
+      } else {
+  	    //cout << "it's empty so new tail is the new head" << endl;
+				startNode = newNodePointer;
+			}
+      //cout << "queue points to new tail" << endl;
+			endNode = newNodePointer;
+      //cout << "all set" << endl;
 			size++;
-			delete newNodePointer;
-  }
+      //cout << "size is now " << size << endl;
+		}
 
 	void dequeue() {
 		if (size < 1) {
 			throw std::underflow_error("You can't delete from an empty queue... Duh!😞");
 		}
-		Node<T>* previous = startNode;
-		Node<T>* current = startNode->get_next();
-		while (current != endNode) {
-			previous = current;
-			current = current->get_next();
+
+		Node<T>* nodeToDelete = startNode;
+
+		// Move startNode to the new head (this will be nullptr if there's only one node)
+		//std::cout << "Setting startNode to next value" << '\n';
+		startNode = startNode->get_next();
+		nodeToDelete->change_next(nullptr);
+        // if the start node was the end node, gotta make that nullptr too
+		if (size == 1) {
+			//std::cout << "Setting endnode to nullptr" << '\n';
+			endNode = nullptr;
 		}
-		endNode = previous;
-		previous.change_next(nullptr);
+		//std::cout << "decrementing size" << '\n';
 		size--;
+		//std::cout << "done!" << '\n';
 	}
 
 	Node<T>* get_start_node() {
@@ -85,5 +94,4 @@ public:
 	int get_size() {
 		return size;
 	}
-
 };
--- homework3/queue_test.cpp
+++ homework3/queue_test.cpp
@@ -4,33 +4,82 @@
 using namespace std;
 
 void testEnqueue() {
-  Queue<string> testEmpty;
-  Node<string>* endNode = testEmpty.get_end_node();
-  Node<string>* startNode = testEmpty.get_start_node();
-  int size = testEmpty.get_size();
+  Queue<string> test;
 
-  assert(endNode == nullptr);
-  assert(startNode == nullptr);
-  assert(size == 0);
-  std::cout << "Empty queue tests passed." << '\n';
+  assert(test.get_end_node() == nullptr);
+  assert(test.get_start_node() == nullptr);
+  assert(test.get_size() == 0);
+  std::cout << "Enqueue with empty queue tests passed." << '\n';
 
-  testEmpty.enqueue("Why ");
+  test.enqueue("Why ");
 
-  endNode = testEmpty.get_end_node();
-  startNode = testEmpty.get_start_node();
-  size = testEmpty.get_size();
-  string startNodeValue = startNode->get_data();
+  assert(test.get_end_node() != nullptr);
+  assert(test.get_start_node() != nullptr);
+  assert(test.get_size() == 1);
+  assert(test.get_start_node()->get_data() == "Why ");
+  assert(test.get_end_node()->get_data() == "Why ");
+  std::cout << "Enqueue with one item tests passed." << '\n';
 
-  /*
-  assert(endNode != nullptr);
-  assert(startNode != nullptr);
-  assert(size == 1);
-  assert(startNodeValue == "Why ");
-  std::cout << "Queue with one item tests passed." << '\n';*/
+  test.enqueue("is ");
+  test.enqueue("this ");
+  test.enqueue("so ");
+  test.enqueue("hard?");
+
+  assert(test.get_end_node() != nullptr);
+  assert(test.get_start_node() != nullptr);
+  assert(test.get_size() == 5);
+  assert(test.get_start_node()->get_data() == "Why ");
+  assert(test.get_end_node()->get_data() == "hard?");
+  std::cout << "Enqueue with multiple items tests passed." << '\n';
+}
+
+void testDequeue() {
+  Queue<string> test;
+
+  test.enqueue("Why ");
+  test.enqueue("is ");
+  test.enqueue("this ");
+  test.enqueue("so ");
+  test.enqueue("hard?");
+
+  assert(test.get_end_node() != nullptr);
+  assert(test.get_start_node() != nullptr);
+  assert(test.get_size() == 5);
+  assert(test.get_start_node()->get_data() == "Why ");
+  assert(test.get_end_node()->get_data() == "hard?");
+  std::cout << "Dequeue with multiple items tests passed." << '\n';
+
+  test.dequeue();
+
+  assert(test.get_end_node() != nullptr);
+  assert(test.get_start_node() != nullptr);
+  assert(test.get_size() == 4);
+  assert(test.get_start_node()->get_data() == "is ");
+  assert(test.get_end_node()->get_data() == "hard?");
+  std::cout << "Dequeue with one less item tests passed." << '\n';
+
+  test.dequeue();
+  test.dequeue();
+  test.dequeue();
+
+  assert(test.get_end_node() != nullptr);
+  assert(test.get_start_node() != nullptr);
+  assert(test.get_size() == 1);
+  assert(test.get_start_node()->get_data() == "hard?");
+  assert(test.get_end_node()->get_data() == "hard?");
+  std::cout << "Dequeue with one last item tests passed." << '\n';
+
+  test.dequeue();
+
+  assert(test.get_end_node() == nullptr);
+  assert(test.get_start_node() == nullptr);
+  assert(test.get_size() == 0);
+  std::cout << "Dequeue with no items tests passed." << '\n';
 }
 
 int main() {
   testEnqueue();
+  testDequeue();
 
   std::cout << "All tests passed!" << '\n';
 }

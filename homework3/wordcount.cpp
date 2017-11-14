b5652a1d27db9f7cd6fe892f0ccca3f319f0c98f
track394 <track394@yahoo.com>
2017-11-13 17:28:00 -0800

    Cleaned up wordcount

--- homework3/wordcount.cpp
+++ homework3/wordcount.cpp
@@ -5,14 +5,11 @@
 
 #include <iostream>
 #include <assert.h>
-#include <vector>
-#include <string>
 using namespace std;
 
 string remove_nonletters(const string& input) {
   string result = input;
   for (int i = 0; i < (signed int)result.length(); i++) {
-    //std::cout << "Current character: " << input[i] << "   Current position: " << i << '\n';
     result[i] = tolower(result[i]);
     if (!isalpha(result[i])) {
       if (i == 0 || (i > 0 && isspace(result[i - 1]))) {
@@ -26,27 +23,10 @@ string remove_nonletters(const string& input) {
   if (result.length() > 0 && !isspace(result[result.length() - 1])) {
     result.append(" ");
   }
-  //std::cout << "Non-letters removed: " << result << '\n';
-  return result;
-}
-
-vector<string> group_words_in_vector(const string& input) {
-  //cout << "Input: " << input << '\n';
-  vector<string> result;
-  int pivot = 0;
-  for (unsigned int i = 0; i < input.length(); i++) {
-    if (isspace(input[i]) || i == input.length() - 1) {
-      //std::cout << "Replacing input: " << input.substr(pivot, i - pivot + 1) << '\n';
-      result.push_back(input.substr(pivot, i - pivot + 1));
-      pivot = i + 1;
-    }
-  }
   return result;
 }
 
 string group_similar_words(string input) {
-  //std::cout << "Input string: " << input << '\n';
-  //string input = "test test test ";
   string result;
   int currentWordPosition = 0;
 
@@ -55,18 +35,12 @@ string group_similar_words(string input) {
       int numberOfWords = 1;
       int repeatFinderPosition = i + 1;
       string currentWord = input.substr(currentWordPosition, i - currentWordPosition + 1);
-      //std::cout << "Current word: " << currentWord << '\n';
 
       for (unsigned int j = i + 1; j < input.length(); j++) {
-        //std::cout << "Current letter of j: " << input[j] << '\n';
-        //std::cout << "Position of counter: " << repeatFinderPosition << " Position of j: " << j << '\n';
         if (isspace(input[j])) {
           if (input.substr(repeatFinderPosition, j - repeatFinderPosition + 1) == currentWord) {
-            //std::cout << "Found a match: " << input.substr(repeatFinderPosition, j - repeatFinderPosition + 1) << "and " << currentWord << '\n';
             numberOfWords++;
-            //std::cout << "Before erasing: " << input << '\n';
             input.erase(repeatFinderPosition, j - repeatFinderPosition + 1);
-            //std::cout << "After  erasing: " << input << '\n';
             j = repeatFinderPosition - 1;
           } else {
             repeatFinderPosition = j + 1;
@@ -74,37 +48,13 @@ string group_similar_words(string input) {
         }
       }
       result += currentWord + to_string(numberOfWords);
-      //std::cout << "New result: " << result << "\n";
       currentWordPosition = i + 1;
     }
   }
   return result;
-
-  // vector<string> result = words;
-  // for (int i = result.size() - 1; i >= 0; i--) {
-  //   wordscount.insert(wordscount.begin(), 1);
-  //   //std::cout << "New word count added." << '\n';
-  //   //std::cout << "Current word: " << words[i] << '\n';
-  //   for (int j = i - 1; j >= 0; j--) {
-  //     if (result[i] == result[j]) {
-  //       //std::cout << "Found match: " << words[i] << "and " << words[j] << '\n';
-  //       //std::cout << "Adding to the front of word count." << '\n';
-  //       wordscount.front()++;
-  //       //std::cout << "Erasing position: " << to_string(j) << '\n';
-  //       //std::cout << "Before erase: " << words[j + 1] << ", " << words[j] << ", " << words[j - 1] << '\n';
-  //       result.erase(result.begin() + j);
-  //       //std::cout << "After erase: " << words[j + 1] << ", " << words[j] << ", " << words[j - 1] << '\n';
-  //       i--;
-  //     }
-  //   }
-  //   result[i] = result[i] + to_string(wordscount.front()) + "\n";
-  //   //std::cout << "Grouping similar words: " << result[i];
-  // }
-  // return result;
 }
 
 string sort_words_large_to_small(string& input) {
-  //string input = "test 4hello 3try 1fall 1doom 2";
   string result;
 
   while (input != "") {
@@ -116,69 +66,28 @@ string sort_words_large_to_small(string& input) {
 
     for (unsigned int i = 0; i < input.length(); i++) {
       char currentLetter = input[i];
-      //std::cout << "Current letter: " << currentLetter << '\n';
 
       if (!isalpha(currentLetter) && !isspace(currentLetter)) {
-        //std::cout << "Current letter is not an alpha and is not a space." << '\n';
         if (stoi(to_string(currentLetter)) > largest) {
           largest = stoi(to_string(currentLetter));
-          //std::cout << "Current letter is an int larger than largest." << '\n';
           largestWord = input.substr(pivot, i - pivot + 1);
           largestPosition = pivot;
           largestSize = i - pivot + 1;
         }
-        //std::cout << "Updating pivot." << '\n';
         pivot = i + 1;
       }
     }
     input.erase(largestPosition, largestSize);
-    //std::cout << "New input: " << input << '\n';
     result += largestWord + "\n";
-    //std::cout << "New result: " << result << '\n';
   }
-  return result;
-
-  // //std::cout << "First word: " << words[0];
-  // for (unsigned int i = 0; i < wordscount.size(); i++) {
-  //   //std::cout << "Comparing word: " << words[i];
-  //   int largest = 1;
-  //   int largestPosition = i;
-  //   for (unsigned int j = i + 1; j < wordscount.size(); j++) {
-  //     //std::cout << "with word: " << words[j];
-  //     if (wordscount[j] > largest) {
-  //       //std::cout << "Found larger value: " << wordscount[j] << " than: " << to_string(largest) << '\n';
-  //       largest = wordscount[j];
-  //       largestPosition = j;
-  //     }
-  //   }
-  //   //std::cout << "Swapping words: " << words[i] << "and " << words[largestPosition];
-  //   string tempStr = words[i];
-  //   words[i] = words[largestPosition];
-  //   //std::cout << words[i];
-  //   words[largestPosition] = tempStr;
-  //   int tempInt = wordscount[i];
-  //   wordscount[i] = wordscount[largestPosition];
-  //   wordscount[largestPosition] = tempInt;
-  //   //std::cout << "Swapped words: " << words[i] << "and " << words[largestPosition];
-  // }
-  // return words;
-}
-
-string convert_vector_to_string(const vector<string> &words) {
-  string result = "";
-  for (string const& word : words) { result += word; }
   return result;
 }
 
 string wordcount(const string& input) {
   string result = input;
   result = remove_nonletters(result);
-  // vector<string> words = group_words_in_vector(result);
-  // vector<int> wordscount;
   result = group_similar_words(result);
   result = sort_words_large_to_small(result);
-  //result = convert_vector_to_string(words);
-  //std::cout << "Final string: " << result << "\n";
   return result;
 }
 

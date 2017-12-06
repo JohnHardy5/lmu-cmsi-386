8f5a5dfe1dee0e44a3711e2ca31b4ea59a581e69
track394 <track394@yahoo.com>
2017-12-05 23:03:41 -0800

    Got change working (tests wont run)

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -2,20 +2,26 @@ module Warmup exposing (..)
 
 import List exposing (..)
 import Basics exposing (..)
---import Tuple exposing (..)
 
 change: Int -> (Int, Int, Int, Int)
-change amount = (0, 0, 0, 0)
+change amount =
+  let
+    a = amount
+    b = a % 25
+    c = b % 10
+    d = c % 5
+  in
+    (,,,) (a // 25) (b // 10) (c // 5) d
 
-stripQuotes: String -> String -> Int
-stripQuotes firstDate secondDate = 0
+stripQuotes: String -> Int
+stripQuotes input = 0
 
 powers: Int -> Int -> List Int
 powers x limit =
-  map (\a -> x ^ a) (range 0 (floor (logBase (toFloat x) (toFloat limit))))
+  List.map (\a -> x ^ a) (range 0 (floor (logBase (toFloat x) (toFloat limit))))
 
 sumOfCubesOfOdds: List Int -> Int
 sumOfCubesOfOdds input = 0
 
-daysBetween: String -> Int
-daysBetween input = 0
+daysBetween: String -> String  -> Int
+daysBetween firstDate secondDate = 0

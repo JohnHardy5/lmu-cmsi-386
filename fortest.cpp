83f26ffd593b59048b855f1cc96e1e00f3276356
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2018-01-06 17:17:59 -0800

    Adding old files

---
+++ fortest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// #include <vector>
+ 
+   int main()
+   {
+    // vector <int> newV1(1,2,3);   
+    // vector <int> newV2(0,0,0);
+    // vector <int> newV3(1,2,3);
+    // vector<vector<int>> a = vector<vector<int>> = vector<vector<int>>(newV1,newV2,newV3); 
+    cout<<"life is lonly";
+    return 0;
+    }
